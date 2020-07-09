44e17af95fc46563c6889e08beb590320ebd10d3
Tracy Shen <34946571+tbs17@users.noreply.github.com>
2020-07-09 15:59:02 -0400

    add morgan

--- node-express/index.js
+++ node-express/index.js
@@ -1,10 +1,14 @@
 // since we already installed the modules to the app, you can directly import it as you do with core modules
 const express=require('express');
 const http=require('http');
+const morgan=require('morgan');//use morgan to log debugging messages
 const hostname='localhost';
 const port=3000;
 // create the app using express framework by using express()
 const app=express();
+app.use(morgan('dev'));//make it a dev version so we can see print outs
+// below is to serve the app using the files under public folder
+app.use(express.static(__dirname+'/public')) //'__dirname' means the root directory of this project
 app.use((req,res,next)=>{
     console.log(req.headers);
     res.statusCode=200;
